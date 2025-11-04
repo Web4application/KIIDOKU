aca77a553b535726a33a2125a4e5de9964f42cfc
Web4 <167559384+Web4application@users.noreply.github.com>
2025-11-04 18:37:46 +0100

    Update Bash.sh

--- Bash.sh
+++ Bash.sh
@@ -7,4 +7,12 @@ mkdir server
 cd server
 npm init -y
 npm install express axios cors dotenv
+git clone https://github.com/Web4application/KIIDOKU.git
+cd KIIDOKU
+npx create-react-app frontend
+mkdir backend
+cd backend
+npm init -y
+npm install express axios cors dotenv
+
 ---
