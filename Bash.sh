5c9b44bcfba21796d9a9c0b09fea6889026f766f
Web4 <167559384+Web4application@users.noreply.github.com>
2025-11-04 18:26:05 +0100

    Update Bash.sh

--- Bash.sh
+++ Bash.sh
@@ -2,3 +2,9 @@ npx create-react-app kiidoku
 cd kiidoku
 npm install tailwindcss framer-motion axios react-router-dom
 npx tailwindcss init
+
+mkdir server
+cd server
+npm init -y
+npm install express axios cors dotenv
+---
